dc2ebd64134cc48189e2cc39b98fd7ae92cc88cb
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2021-07-10 09:20:20 +0530

    added;UC1

--- hello.java
+++ hello.java
@@ -3,5 +3,14 @@ class hello
 public static  void main(String[] args)
 {
 System.out.println("hello word");
+
+
+int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			 System.out.println("Employee is Absent");
+	}
 }
-}
+
